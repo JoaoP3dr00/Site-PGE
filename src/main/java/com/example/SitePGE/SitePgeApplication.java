206bbba5fb1bdf39f3809e74a25b110677c553c9
joao <rjoao.pedro05@gmail.com>
2024-07-24 15:47:33 -0400

    first commit

---
+++ src/main/java/com/example/SitePGE/SitePgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SitePGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePgeApplication.class, args);
+	}
+
+}
